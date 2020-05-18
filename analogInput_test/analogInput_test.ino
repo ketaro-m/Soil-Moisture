cdfde2ea37d44d1b2070ac2d42ccbbef4d97e987
Keitaro Murakami <keitaro.mrkm@berkeley.edu>
2020-05-18 04:21:06 -0700

    confirm analog input

---
+++ analogInput_test/analogInput_test.ino
@@ -0,0 +1,14 @@
+const int readPin = 13;
+
+float value = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  value = analogRead(readPin);
+  Serial.println(value);
+  delay(500);
+}
