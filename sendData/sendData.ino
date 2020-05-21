9f75ab308ee29da43123bffd07ee8f19f2a27437
Keitaro Murakami <keitaro.mrkm@berkeley.edu>
2020-05-21 06:27:04 -0700

    confirm wifi connection

---
+++ sendData/sendData.ino
@@ -0,0 +1,42 @@
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+// Use Virtual pin 5 for uptime display
+#define PIN_UPTIME V5
+
+// This function tells Arduino what to do if there is a Widget
+// which is requesting data for Virtual Pin (5)
+BLYNK_READ(PIN_UPTIME)
+{
+  // This command writes Arduino's uptime in seconds to Virtual Pin (5)
+  Blynk.virtualWrite(PIN_UPTIME, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
